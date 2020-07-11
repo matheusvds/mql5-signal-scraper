b20b33027dab1176afce44760d9777b376e03627
Matheus Sousa <matheusvds@gmail.com>
2020-07-10 23:22:29 -0300

    makes request to mql5

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
+import requests
 import csv
 
 filename = 'signals.csv'
 
 f = csv.writer(open(filename, 'w', newline=''))
 f.writerow(['Name', 'Price', 'Growth', 'Monthly Growth', 'Subs', 'Funds', 'Weeks', 'Trades', 'Winrate', 'DD'])
+
+html = requests.get('https://www.mql5.com/en/signals/mt4/list/')
