8c9544f9ad80602085f2566c9f33197681033ee9
Matheus Sousa <matheusvds@gmail.com>
2020-07-25 12:14:29 -0300

    finds the amount of pages

--- scraper.py
+++ scraper.py
@@ -9,3 +9,5 @@ f.writerow(['Name', 'Price', 'Growth', 'Monthly Growth', 'Subs', 'Funds', 'Weeks
 
 html = requests.get('https://www.mql5.com/en/signals/mt4/list/')
 soup = BeautifulSoup(html.text, 'html.parser')
+
+amountOfPages = int(soup.find(class_="paginatorEx").findAll('a')[-1].text)
